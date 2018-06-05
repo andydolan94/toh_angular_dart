162c7e128280356f14bb78d8c1c00ae8fea37076
Andrew Dolan <andrewdolan@hotmail.co.nz>
2018-06-05 18:03:46 +1200

    Part way through the Services section

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,6 +2,7 @@ import 'package:angular/angular.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
 	selector: 'my-app',
@@ -14,6 +15,10 @@ class AppComponent {
 	final title = 'Tour of Heroes';
 	List<Hero> heroes = mockHeroes;
 	Hero selected;
+
+	// TODO: YOU ARE HERE
+	final HeroService _heroService;
+	AppComponent(this._heroService);
 
 	// The onSelect method that occurs when the user clicks on a hero
 	void onSelect(Hero hero) => selected = hero;
---
+++ lib/src/hero_service.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+import 'hero.dart';
+import 'mock_heroes.dart';
+
+@Injectable()
+class HeroService {
+	// Hero Service code here
+}
