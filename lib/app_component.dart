c058dcc79b0e943c0091e6d8608cf8db4f188061
Andrew Dolan <andrewdolan@hotmail.co.nz>
2018-06-05 16:12:27 +1200

    completed the hero editor

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,22 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'hero.dart';
 
 @Component(
 	selector: 'my-app',
 	template: '''
   		<h1>{{title}}</h1>
-  		<h2>{{hero}}</h2>
+  		<h2>{{hero.name}}</h2>
+		<div><label>id: </label>{{hero.id}}</div>
+		<div>
+			<label>name: </label>
+			<input [(ngModel)]="hero.name" placeholder="name">
+		</div>
 	''',
+	directives: [formDirectives],
 )
+
 class AppComponent {
 	final title = 'Tour of Heroes';
-	var hero = 'Windstorm';
+	Hero hero = new Hero(1, 'Windstorm');
 }
