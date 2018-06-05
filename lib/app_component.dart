7c50639dd5eb86dca8240dc6159709104530bbbf
Andrew Dolan <andrewdolan@hotmail.co.nz>
2018-06-05 18:49:07 +1200

    Services completed

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,24 +1,31 @@
 import 'package:angular/angular.dart';
 import 'src/hero.dart';
-import 'src/mock_heroes.dart';
 import 'src/hero_component.dart';
 import 'src/hero_service.dart';
+import 'dart:async';
 
 @Component(
 	selector: 'my-app',
 	templateUrl: 'app_component.html',
 	styleUrls: ['app_component.css'],
 	directives: [coreDirectives, HeroComponent],
+	providers: [const ClassProvider(HeroService)],
 )
 
-class AppComponent {
+class AppComponent implements OnInit {
 	final title = 'Tour of Heroes';
-	List<Hero> heroes = mockHeroes;
+	final HeroService _heroService;
+
+	List<Hero> heroes;
 	Hero selected;
 
-	// TODO: YOU ARE HERE
-	final HeroService _heroService;
 	AppComponent(this._heroService);
+
+	void ngOnInit() => _getHeroes();
+
+	Future<void> _getHeroes() async {
+		heroes = await _heroService.getAll();
+	}
 
 	// The onSelect method that occurs when the user clicks on a hero
 	void onSelect(Hero hero) => selected = hero;
--- lib/src/hero_service.dart
+++ lib/src/hero_service.dart
@@ -1,9 +1,14 @@
 import 'package:angular/angular.dart';
+import 'dart:async';
 
 import 'hero.dart';
 import 'mock_heroes.dart';
 
 @Injectable()
 class HeroService {
-	// Hero Service code here
+	Future<List<Hero>> getAll() async => mockHeroes;
+
+	Future<List<Hero>> getAllSlowly() {
+		return new Future.delayed(const Duration(seconds: 2), getAll);
+	}
 }
